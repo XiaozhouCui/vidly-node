7ff001558b03e62f57a68df6e726876ba0a919fa
Joe Cui <joe.cui@bpanz.com>
2020-05-17 16:17:12 +1000

    Added CRUD operations on Genres

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const Joi = require("@hapi/joi");
+const app = express();
+
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 const genres = [
-  { id: 1, name: "genre1" },
-  { id: 2, name: "genre2" },
-  { id: 3, name: "genre3" },
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
 ];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
 });
 
 app.get("/api/genres", (req, res) => {
@@ -25,8 +43,29 @@ app.get("/api/genres/:id", (req, res) => {
   res.send(genre);
 });
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Listening to port ${port}...`);
+app.put("/api/genres/:id", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre not found");
+  genre.name = req.body.name;
+  res.send(genre);
 });
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(404).send("Genre already deleted");
+  index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening to port ${port}...`));
